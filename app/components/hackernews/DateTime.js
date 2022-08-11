0f02061423a9669ac98b4e0fcc787cc1d4420eba
Franziska Eder <f.eder@gmx.info>
2022-08-11 16:17:44 +0200

    fix

--- app/components/hackernews/DateTime.js
+++ app/components/hackernews/DateTime.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default function DateTime({timestamp}) {
+export default function DateTime({ timestamp }) {
   let milliseconds = new Date(timestamp*1000)
   let date = milliseconds.toLocaleDateString('de-DE')
   let time = milliseconds.toLocaleTimeString('de-DE')
